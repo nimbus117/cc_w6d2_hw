3ad23ba2201e1fe5403d853d52455b3d66fcbfaa
nimbus117 <mail@nimbus117.co.uk>
2018-09-04 17:59:24 +0100

    refactored findPopularDinosaur

--- models/park.js
+++ models/park.js
@@ -14,16 +14,22 @@ Park.prototype.removeDinosaur = function(dinosaurToRemove) {
 };
 
 Park.prototype.findPopularDinosaur = function() {
-  this.dinosaurs.sort(function(a, b) {
-    let compare = 0;
-    if (a.guestsAttractedPerDay > b.guestsAttractedPerDay) {
-      compare = 1;
-    }
-    else if (b.guestsAttractedPerDay > a.guestsAttractedPerDay) {
-      compare = -1;
-    }
-    return compare;
-  })
+  // this.dinosaurs.sort(function(a, b) {
+  //   let compare = 0;
+  //   if (a.guestsAttractedPerDay > b.guestsAttractedPerDay) {
+  //     compare = 1;
+  //   }
+  //   else if (b.guestsAttractedPerDay > a.guestsAttractedPerDay) {
+  //     compare = -1;
+  //   }
+  //   return compare;
+  // });
+
+  // this.dinosaurs.sort(function(a, b) {
+  //   return a.guestsAttractedPerDay - b.guestsAttractedPerDay;
+  // });
+
+  this.dinosaurs.sort((a,b) => a.guestsAttractedPerDay - b.guestsAttractedPerDay);
   return this.dinosaurs[this.dinosaurs.length - 1];
 }
 
